5deb8cb7ec5ee9b2f0c617dbeaee578b23505ff6
caio da silva <caio@debian.caio>
2026-07-22 18:17:58 -0300

    build: Configura electron-builder com ícone personalizado

--- agent/src/main/main.js
+++ agent/src/main/main.js
@@ -14,6 +14,7 @@ function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
+    icon: path.join(__dirname, '../../icon.png'),
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false
